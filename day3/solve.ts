606331e4e187b4ee68b1777b2a232a02221f944b
Maria G <ruiined@gmail.com>
2023-12-09 22:04:26 +0000

    feat: day3 part2 solution

--- day3/solve.ts
+++ day3/solve.ts
@@ -1,27 +1,26 @@
-import { sum } from 'lodash';
+import { sum, keys, values } from 'lodash';
 import { parseLines, solve } from '../utils';
 
 const symbolRegex = /[-’/`~!#*$@_%+=,^&(){}[\]|;:”<>?\\]/g;
 
-function part1(_input: string[]) {
-  const symbolCoords = _input.reduce((acc, line, colIdx) => {
-    line.split('').forEach((char, idx) => {
-      if (symbolRegex.test(char)) {
-        acc[colIdx] = acc[colIdx] || [];
-        acc[colIdx].push(idx);
+const getCoords = (arr: string[], regex: RegExp) => {
+  return arr.reduce((acc, row, col) => {
+    row.split('').forEach((char, i) => {
+      if (regex.test(char)) {
+        acc[col] = acc[col] || [];
+        acc[col].push(i);
       }
     });
     return acc;
   }, {});
+};
 
-  const numbers = _input.reduce((acc, line, colIdx) => {
-    const arr = line.split('');
-    const digits: string[] = [];
+function part1(_input: string[]) {
+  const symbolCoords = getCoords(_input, symbolRegex);
 
-    const touches = (n: number) =>
-      symbolCoords[colIdx]?.includes(n) ||
-      symbolCoords[colIdx - 1]?.includes(n) ||
-      symbolCoords[colIdx + 1]?.includes(n);
+  const numbers = _input.reduce((acc, row, col) => {
+    const arr = row.split('');
+    const digits = [];
 
     arr.forEach((s, idx) => {
       if (/[0-9]/.test(s)) {
@@ -32,8 +31,10 @@ function part1(_input: string[]) {
       if (!digits.length) return;
 
       const numberIdxs = digits.map((_, i) => i + idx - digits.length);
-      const touchesSymbol = numberIdxs.some(
-        (n) => touches(n) || touches(n - 1) || touches(n + 1),
+      const touchesSymbol = numberIdxs.some((n) =>
+        [n - 1, n, n + 1].some((r) =>
+          [col - 1, col, col + 1].some((c) => symbolCoords[c]?.includes(r)),
+        ),
       );
 
       if (touchesSymbol) {
@@ -49,8 +50,73 @@ function part1(_input: string[]) {
   return sum(numbers);
 }
 
+const offsets = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 0],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+];
+
+const getZones = (x: number, y: number) => {
+  return offsets
+    .map(([dx, dy]) => [x + dx, y + dy])
+    .filter(([x, y]) => x >= 0 && y >= 0);
+};
+
 function part2(_input: string[]) {
-  return 'part2';
+  const asteriskCoords = getCoords(_input, /[*]/);
+  const asteriskZones = keys(asteriskCoords)
+    .map((col) =>
+      asteriskCoords[col].map((row: number) => ({
+        id: `${col}_${row}`,
+        coords: getZones(Number(col), row),
+      })),
+    )
+    .flat();
+
+  const numbers = _input.reduce((acc, row, col) => {
+    const arr = row.split('');
+    const digits = [];
+
+    arr.forEach((s, idx) => {
+      if (/[0-9]/.test(s)) {
+        digits.push(s);
+        if (idx !== arr.length - 1) return;
+      }
+
+      if (!digits.length) return;
+
+      const numberIdxs = digits.map((_, i) => [col, i + idx - digits.length]);
+
+      const asteriskNearNumbers = asteriskZones.filter(
+        (zone) =>
+          zone.coords?.some(
+            (c: number[]) =>
+              numberIdxs?.some((n) => n[0] === c[0] && n[1] === c[1]),
+          ),
+      );
+
+      asteriskNearNumbers?.forEach(({ id }) => {
+        acc[id] = acc[id] || [];
+        acc[id].push(parseInt(digits.join('')));
+      });
+
+      digits.length = 0;
+    });
+
+    return acc;
+  }, {});
+
+  return sum(
+    values(numbers)
+      .filter((arr: string[]) => arr.length > 1)
+      .map((n: number[]) => n.reduce((a, b) => a * b, 1)),
+  );
 }
 
-solve({ part1, test1: 4361, part2, parser: parseLines() });
+solve({ part1, test1: 4361, part2, test2: 467835, parser: parseLines() });
