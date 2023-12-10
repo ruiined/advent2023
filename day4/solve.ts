c5e6198766fc9efed567d7a620cf1dce7cf46437
Maria G <ruiined@gmail.com>
2023-12-10 00:36:36 +0000

    feat: day 4 part 2 solution

--- day4/solve.ts
+++ day4/solve.ts
@@ -1,30 +1,54 @@
+import { sum } from 'lodash';
 import { parseLines, solve } from '../utils';
+
+const getCardPoints = (wins: number): number => {
+  if (!wins) return 0;
+
+  return Array.from({ length: wins })
+    .map((_, i) => Math.pow(2, i))
+    .at(-1);
+};
+
+const getWinCount = (card: string): number => {
+  const [_, winningNumbers, myNumbers] = card
+    .split(/[:|]/)
+    .map((c) => c.trim().split(' ').filter(Boolean));
+
+  return winningNumbers.reduce((acc, n) => {
+    if (myNumbers.includes(n)) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+};
 
 const part1 = (_input: string[]) => {
   return _input.reduce((acc, card) => {
-    const [_, winningNumbers, myNumbers] = card
-      .split(/[:|]/)
-      .map((c) => c.trim().split(' ').filter(Boolean));
-
-    let wins = 0;
-
-    winningNumbers.forEach((n) => {
-      if (myNumbers.includes(n)) {
-        wins++;
-      }
-    });
-
-    if (wins) {
-      const points = Array.from({ length: wins }).map((_, i) => Math.pow(2, i));
-      acc += points.at(-1);
-    }
+    const wins = getWinCount(card);
+    acc += getCardPoints(wins);
 
     return acc;
   }, 0);
 };
 
 const part2 = (_input: string[]) => {
-  return 0;
+  const cards = _input.reduce((acc, card, idx) => {
+    const wins = getWinCount(card);
+
+    Array.from({ length: wins }).forEach((_, i) => {
+      const nextIdx = idx + i + 1;
+      acc[nextIdx] = acc[nextIdx] || 1;
+      acc[nextIdx] += acc[idx] ?? 1;
+    });
+
+    if (!acc[idx]) {
+      acc[idx] = 1;
+    }
+
+    return acc;
+  }, []);
+
+  return sum(cards);
 };
 
-solve({ part1, test1: 13, part2, test2: 999, parser: parseLines() });
+solve({ part1, test1: 13, part2, test2: 30, parser: parseLines() });
